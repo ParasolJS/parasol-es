715e1d3be59c0664ff6175c707ce773ef929d57c
Josh Jacobson <joshuah.jacobson@gmail.com>
2018-07-11 17:45:19 -0600

    redo ParaVis function, extensible to n plots

--- dist/parasol.js
+++ dist/parasol.js
@@ -1,6 +1,9 @@
 
+// NOTE: somehow require d3, parcoords, underscore, jquery, ...
 
-// function template //
+// ----------
+// template
+// ----------
 // function description
 // var fname = function fname(global vars) {
 //   return function (local vars) {
@@ -17,12 +20,12 @@
 // .grid()
 // .linked([plot_list])
 // .link_reset(brushes, marks, all)
-// .cluster(k=3, group="both")
-// .weight({weights})
+// .cluster(k=3, group="both", color_palette)
+// .aggregate({weights})
 // .hide([axes_list])
 // .show([axes_list])
 // .utopia_line([optimize_list])
-// .uncertainty_mode(1,2,3,4)
+// //.uncertainty_mode(1,2,3,4)
 //
 // keep_selected(selection = (brushed, marked, both))
 // remove_selected(selection = (brushed, marked, both))
@@ -43,25 +46,67 @@
 
 // constructor:
 // initializes two independent ParCoords vars and retuns them in an array
-var ParaVis = function ParaVis(dataset, objectives=null) {
-  var pc1 = ParCoords()("#plot1")
-      .data(dataset)
-      .alpha(0.4)
-      .alphaOnBrushed(0.1)
-      .render()
-      .reorderable()
-      .mode("queue")
-      .brushMode("1D-axes");
+var ParaVis = function ParaVis(dataset, partition, n, layout=null) {
+  // dataset: may be either an object or an array, all values should be the same format
+  // partition: object indicating which plots a variable should be included in; formatted as partition = { "var": [pids] }
+  // n: number of plots to create
+  // layout: object specifying plot dimensions; default is
+  //    layout = {height: "200px", width: "100%"}
+  console.log(partition);
 
-  var pc2 = ParCoords()("#plot2")
-      .data(dataset)
-      .alpha(0.4)
-      .alphaOnBrushed(0.1)
-      .render()
-      .reorderable()
-      .mode("queue")
-      .brushMode("1D-axes");
+  // setup array of arrays to store hidden axes for all plots
+  var hidden = Array.from({length: n}, (pid) => []);
 
-  var plot_list = [pc1, pc2]
-  return(plot_list);
+  // update hidden variables based on partition
+  Object.entries(partition).forEach(
+    ([axis, pids]) => {
+      // identify pids in which an axis should be hidden
+      var hidden_pids = _.difference(_.range(n), pids);
+      // append axis name to hidden[pid] array
+      hidden_pids.forEach(
+        (pid) => { hidden[pid].push(axis) }
+      )
+    }
+  );
+
+  // hide axis from all plots if not found in partition
+  var extraneous_vars = _.difference(Object.keys(dataset[0]), Object.keys(partition));
+  hidden.forEach(
+    (hidden_pid) => {
+      hidden_pid.push(extraneous_vars)
+    })
+  console.log(hidden);
+
+  // setup DOM layout if necessary
+  if(!layout){
+    layout = {height: "200px", width: "100%"}
+  }
+  console.log(layout);
+
+  // build pc plots as array elements
+  var plots = Array.from({length: n},
+    (pid, idx) => {
+      layout["id"] = "pc"+idx
+      layout["class"] = "parcoords"
+      console.log(layout);
+      // create new div for each plot
+
+      // NOTE: try this with the updated version of jQuery
+      var plot_div = document.createElement("div");
+      $(plot_div).attr(layout);
+      // $(plot_div).appendTo( $( ".container" ) );
+      // populate array id with function and plot
+      // pid = ParCoords()('#pc'+idx)
+      //   .data(dataset)
+      //   .hideAxis(hidden[idx])
+      //   .alpha(0.4)
+      //   .alphaOnBrushed(0.1)
+      //   .render()
+      //   .reorderable()
+      //   .mode("queue")
+      //   .brushMode("1D-axes");
+  });
+  console.log(plots);
+
+  // return(plot_list);
 }
